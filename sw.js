e2f6154669f78f8bf655588000eab34d93d2e874
Ventsharm <94763309+Ventsharm@users.noreply.github.com>
2026-01-18 04:34:04 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "ventsharm-donate-v1";
+const OFFLINE_URL = "/donate-site/";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c =>
+      c.addAll([OFFLINE_URL])
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(OFFLINE_URL))
+  );
+});
